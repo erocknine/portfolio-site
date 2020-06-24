dd211aa6e9d1c5ffaac598d7e27f38e887546720
Erocknine <echeungsk@gmail.com>
2020-06-24 00:24:35 -0400

    redesigned color scheme

--- src/components/profile.js
+++ src/components/profile.js
@@ -29,18 +29,15 @@ export default function Profile() {
 
       <div id="profile" className="profile-text">
         <ScrollAnimation offset={25} animateIn="fadeInDown" animateOnce={true} delay={200}><h2 className="text">Eric Sai Kit Cheung</h2></ScrollAnimation>
-        <ScrollAnimation animateIn="flipInX" animateOnce={true} delay={200}>
-          <p className="text"><Icon name="angle double right"/>Full-stack Software Engineer</p>
-          <p className="text"><Icon name="angle double right"/>Trilingual, fluent in English and Chinese, and conversational in Japanese</p>
-
-          <p className="text desc">I am constantly charmed by the complexities of programming and coding technologies, and amazed by what's been created before me. My goal for every project is to be visually appealing yet functional, creative yet intuitive.</p>
-            
-          <p className="text desc">I love traveling, gaming, and eating as many different kinds of food as possible! I’ve taught English in Japan, owned and operated a food business in Hawaii, and self-published a novel while in NYC.</p>
-        </ScrollAnimation>
+        
+        <ScrollAnimation animateIn="flipInX" animateOnce={true} delay={200}><p className="text"><Icon name="angle double right"/>Full-stack Software Engineer</p></ScrollAnimation>
+        <ScrollAnimation animateIn="flipInX" animateOnce={true} delay={200}><p className="text"><Icon name="angle double right"/>Trilingual, fluent in English and Chinese, and conversational in Japanese</p></ScrollAnimation>
+        <ScrollAnimation animateIn="flipInX" animateOnce={true} delay={200}><p className="text desc">I am constantly charmed by the complexities of programming and coding technologies, and amazed by what's been created before me. My goal for every project is to be visually appealing yet functional, creative yet intuitive.</p></ScrollAnimation> 
+        <ScrollAnimation animateIn="flipInX" animateOnce={true} delay={200}><p className="text desc">I love traveling, gaming, and eating as many different kinds of food as possible! I’ve taught English in Japan, owned and operated a food business in Hawaii, and self-published a novel while in NYC.</p></ScrollAnimation>
 
         <ScrollAnimation offset={25} animateIn="fadeInDown" animateOnce={true}><h3 className="text">{`// Technical Skills`}</h3></ScrollAnimation>
-        <ScrollAnimation offset={25} animateIn="flipInX" animateOnce={true} delay={100}><p className="text"><Icon name="angle double right"/>Languages & Frameworks - React, Redux, Javascript, Ruby, Rails, NodeJS</p></ScrollAnimation>
-        <ScrollAnimation offset={25} animateIn="flipInX" animateOnce={true} delay={100}><p className="text"><Icon name="angle double right"/>Databases - Express, MongoDB, PostgreSQL, SQL</p></ScrollAnimation>
+        <ScrollAnimation offset={25} animateIn="flipInX" animateOnce={true} delay={100}><p className="text"><Icon name="angle double right"/>Languages & Frameworks - React, Redux, Javascript, Ruby, Rails, NodeJS, Express</p></ScrollAnimation>
+        <ScrollAnimation offset={25} animateIn="flipInX" animateOnce={true} delay={100}><p className="text"><Icon name="angle double right"/>Databases - MongoDB, PostgreSQL, SQL</p></ScrollAnimation>
         <ScrollAnimation offset={25} animateIn="flipInX" animateOnce={true} delay={100}><p className="text"><Icon name="angle double right"/>Style - CSS, Sass, Bootstrap, Material IO, Semantic UI </p></ScrollAnimation>
       </div>
     </section>
--- src/components/resume.js
+++ src/components/resume.js
@@ -16,9 +16,8 @@ export default function Resume() {
             <div>
               <ScrollAnimation offset={25} animateIn="flipInX" animateOnce={true} delay={200}><h3>Bigeye Sushi</h3></ScrollAnimation>
               <ScrollAnimation offset={25} animateIn="fadeIn" animateOnce={true} delay={200}><p className="resume-text-sub"><strong>Assistant Manager, Brooklyn, NY, October 2018 - October 2019</strong></p></ScrollAnimation>
-              <ScrollAnimation offset={25} animateIn="fadeIn" animateOnce={true} delay={200}><p className="resume-text-desc"><Icon name="angle double right"/>Managed staff, EOD financials, inventory, pop-up events, and food prep at Sushi restaurant in downtown Brooklyn</p></ScrollAnimation>
+              <ScrollAnimation offset={25} animateIn="fadeIn" animateOnce={true} delay={200}><p className="resume-text-desc"><Icon name="angle double right"/>Managed staff, EOD financials, inventory, pop-up events, and food prep at Sushi restaurant in downtown Brooklyn.</p></ScrollAnimation>
             </div>
-            
             <div>
               <ScrollAnimation offset={25} animateIn="flipInX" animateOnce={true} delay={200}><h3>Banzai Phresh</h3></ScrollAnimation>
               <ScrollAnimation offset={25} animateIn="fadeIn" animateOnce={true} delay={200}> <p className="resume-text-sub"><strong>Owner/Sole Proprietor, Honolulu, HI, December 2017 - June 2018</strong></p></ScrollAnimation>
@@ -34,7 +33,7 @@ export default function Resume() {
             <div>
               <ScrollAnimation offset={25} animateIn="flipInX" animateOnce={true} delay={200}><h3>Zipcar</h3></ScrollAnimation>
               <ScrollAnimation offset={25} animateIn="fadeIn" animateOnce={true} delay={200}><p className="resume-text-sub"><strong>Fleet Associate, Brooklyn, NY, 2015</strong></p></ScrollAnimation>
-              <ScrollAnimation offset={25} animateIn="fadeIn" animateOnce={true} delay={200}><p className="resume-text-desc"><Icon name="angle double right"/>Accomplished daily tasks involving handling and managing Zipcars all around Brooklyn. Issues included fixing flat tires, dead batteries, oil changes, and retrieving cars.</p></ScrollAnimation>
+              <ScrollAnimation offset={25} animateIn="fadeIn" animateOnce={true} delay={200}><p className="resume-text-desc"><Icon name="angle double right"/>Accomplished daily tasks of maintaining and managing Zipcars all around Brooklyn. Issues included exploring unknown problems, fixing flat tires, dead batteries, oil changes, and retrieving cars.</p></ScrollAnimation>
             </div>
             <div>
               <ScrollAnimation offset={25} animateIn="flipInX" animateOnce={true} delay={200}><h3>Kings of Fortune</h3></ScrollAnimation>
