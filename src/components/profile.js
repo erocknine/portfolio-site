b7a8b7121e902b8993b90272f2ed3299fb2fec20
Erocknine <echeungsk@gmail.com>
2020-07-07 09:42:25 -0400

    removed scroll animation from profile

--- src/components/profile.js
+++ src/components/profile.js
@@ -28,17 +28,17 @@ export default function Profile() {
       </div>
 
       <div id="profile" className="profile-text">
-        <ScrollAnimation offset={25} animateIn="fadeInDown" animateOnce={true} delay={200}><h2 className="text">Eric Sai Kit Cheung</h2></ScrollAnimation>
+        <h2 className="text">Eric Sai Kit Cheung</h2>
         
-        <ScrollAnimation animateIn="flipInX" animateOnce={true} delay={200}><p className="text"><Icon name="angle double right"/>Full-stack Software Engineer</p></ScrollAnimation>
-        <ScrollAnimation animateIn="flipInX" animateOnce={true} delay={200}><p className="text"><Icon name="angle double right"/>Trilingual, fluent in English and Chinese, and conversational in Japanese</p></ScrollAnimation>
-        <ScrollAnimation animateIn="flipInX" animateOnce={true} delay={200}><p className="text desc">I love to build aesthetically pleasing projects, find creative solutions to problems, and am an avid food adventurer and cat instagram follower. My goal for every project is to be visually appealing yet functional, creative yet intuitive.</p></ScrollAnimation> 
-        <ScrollAnimation animateIn="flipInX" animateOnce={true} delay={200}><p className="text desc">I love traveling, gaming, and eating as many different kinds of food as possible! I’ve taught English in Japan, owned and operated a food business in Hawaii, and self-published a novel while in NYC.</p></ScrollAnimation>
+        <p className="text"><Icon name="angle double right"/>Full-stack Software Engineer</p>
+        <p className="text"><Icon name="angle double right"/>Trilingual, fluent in English and Chinese, and conversational in Japanese</p>
+        <p className="text desc">I love to build aesthetically pleasing projects, find creative solutions to problems, and am an avid food adventurer and cat instagram follower. My goal for every project is to be visually appealing yet functional, creative yet intuitive.</p> 
+        <p className="text desc">I love traveling, gaming, and eating as many different kinds of food as possible! I’ve taught English in Japan, owned and operated a food business in Hawaii, and self-published a novel while in NYC.</p>
 
-        <ScrollAnimation offset={25} animateIn="fadeInDown" animateOnce={true}><h3 className="text">{`// Technical Skills`}</h3></ScrollAnimation>
-        <ScrollAnimation offset={25} animateIn="flipInX" animateOnce={true} delay={100}><p className="text"><Icon name="angle double right"/>Languages & Frameworks - React, Redux, Javascript, Ruby, Rails, NodeJS, Express, HTML</p></ScrollAnimation>
-        <ScrollAnimation offset={25} animateIn="flipInX" animateOnce={true} delay={100}><p className="text"><Icon name="angle double right"/>Databases - MongoDB, PostgreSQL, SQL</p></ScrollAnimation>
-        <ScrollAnimation offset={25} animateIn="flipInX" animateOnce={true} delay={100}><p className="text"><Icon name="angle double right"/>Style - CSS, SASS, Bootstrap, Material IO, Semantic UI </p></ScrollAnimation>
+        <h3 className="text">{`// Technical Skills`}</h3>
+        <p className="text"><Icon name="angle double right"/>Languages & Frameworks - React, Redux, Javascript, Ruby, Rails, NodeJS, Express, HTML</p>
+        <p className="text"><Icon name="angle double right"/>Databases - MongoDB, PostgreSQL, SQL</p>
+        <p className="text"><Icon name="angle double right"/>Style - CSS, SASS, Bootstrap, Material IO, Semantic UI </p>
       </div>
     </section>
   )
