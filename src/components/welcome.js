4a3758e6df369cd10da4e124eb054464cd0f1626
erocknine <erock.esquire@gmail.com>
2021-01-22 18:54:43 -0500

    Removed uifx library and used javascript audio function

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,31 +1,42 @@
-import React, { useState, Fragment } from 'react'
-import UIfx from 'uifx';
-import Sound from '../sounds/switch-click.mp3';
+import React, { useState, Fragment } from "react";
 
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from "react-router-dom";
 
 export default function Welcome(props) {
+  const location = useLocation();
+  const { handleWelcome } = props;
+  const [sliders, setSliders] = useState(false);
+  const [pressed, setPressed] = useState(false);
 
-  let location = useLocation();
-  const { handleWelcome } = props
-  const [sliders, setSliders] = useState(false)
-  const [pressed, setPressed] = useState(false)
-
-  const switchClick = new UIfx(Sound);
+  const switchClick = new Audio("/sounds/switch-click.mp3");
 
   const handlePress = () => {
-    setPressed(true)
-    switchClick.play()
-  }
+    setPressed(true);
+    switchClick.play();
+  };
 
   return (
     <section className="welcome-section">
-      <Link to={`${location.pathname}`} className={pressed ? "signature pressed":"signature"} onMouseDown={() => handlePress()} onMouseUp={() => {setSliders(true); setPressed(false); handleWelcome()}} onMouseLeave={() => setPressed(false)}>
+      <Link
+        to={`${location.pathname}`}
+        className={pressed ? "signature pressed" : "signature"}
+        onMouseDown={() => handlePress()}
+        onMouseUp={() => {
+          setSliders(true);
+          setPressed(false);
+          handleWelcome();
+        }}
+        onMouseLeave={() => setPressed(false)}
+      >
         <h1>張</h1>
       </Link>
-      
-      {sliders && <Fragment><div className="welcome-panel-left"></div>
-      <div className="welcome-panel-right"></div></Fragment>}
+
+      {sliders && (
+        <Fragment>
+          <div className="welcome-panel-left"></div>
+          <div className="welcome-panel-right"></div>
+        </Fragment>
+      )}
     </section>
-  )
+  );
 }
